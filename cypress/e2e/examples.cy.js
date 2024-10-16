f492011feaf15a70dc18f485ea1aa3614a320bb1
kiiaseilonen <kiia.seilonen@hotmail.com>
2024-10-16 12:45:07 +0300

    Grudgelist tests added

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -25,10 +25,57 @@ describe('Various examples', () => {
         cy.location("pathname").should("equal", "/best-practices")
     })
 
-    it.only('intercepts', () => {
+    it('intercepts', () => {
         cy.intercept("POST", 'http://localhost:3000/examples', {
             fixture: 'example.json'
         })
         cy.getDataTest('post-button').click();
+    })
+
+    it.only('grudges', () => {
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 0)
+        })
+
+        cy.getDataTest('clear-grudges-button').should('not.exist')
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Add Some Grudges')
+
+        cy.getDataTest('grudge-input').within(() => {
+            cy.get('input').type('First grudge on the list')
+        })
+        cy.getDataTest('add-grudge-button').click()
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 1)
+        })
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Grudges')
+
+
+        cy.getDataTest('grudge-input').within(() => {
+            cy.get('input').type('Second grudge on the list')
+        })
+        cy.getDataTest('add-grudge-button').click()
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 2)
+            cy.get('li').its(0).should('contains.text', 'First grudge on the list')
+            cy.get('li').its(1).should('contains.text', 'Second grudge on the list')
+        })
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').its(0).within(() => {
+                cy.get('button').click()
+            })
+            cy.get('li').should('have.length', 1)  
+        })  
+
+        cy.getDataTest('clear-grudges-button').click()
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 0)
+        })
+
     })
 })
